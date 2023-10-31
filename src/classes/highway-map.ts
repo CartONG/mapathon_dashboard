6edb9a1fd66cd50948559102d6ac1079b5da0b7b
Jean-Baptiste REGAZZONI <jean-baptiste@revolucy.fr>
2023-10-31 16:44:14 +0100

    :package: Update tile layer URL

--- src/classes/highway-map.ts
+++ src/classes/highway-map.ts
@@ -5,9 +5,9 @@ import * as L from "leaflet";
 import { HighwayLayers } from "./highway-layers";
 
 const attribution = [
-  'Map tiles by <a href="https://stamen.com" target="_blank">Stamen Design</a>,',
-  '<a href="https://creativecommons.org/licenses/by/3.0" target="_blank">CC BY 3.0</a> <br>Map data &copy; <a href="http://www.openstreetmap.org" target="_blank">OpenStreetMap</a> contributors,',
-  '<a href="http://opendatacommons.org/licenses/odbl/1.0/" target="_blank">ODbL</a>.',
+  '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ',
+  'Tiles style by <a href="https://www.hotosm.org/" target="_blank">Humanitarian OpenStreetMap Team</a>',
+  'hosted by <a href="https://openstreetmap.fr/" target="_blank">OpenStreetMap France</a>'
 ];
 
 // Options of the highway map
@@ -16,7 +16,7 @@ const HIGHWAY_MAP_OPTIONS = {
   zoom: 10,
   layers: [
     L.tileLayer(
-      "https://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}.{ext}",
+      "https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.{ext}",
       {
         attribution: attribution.join(" "),
         subdomains: "abcd",
